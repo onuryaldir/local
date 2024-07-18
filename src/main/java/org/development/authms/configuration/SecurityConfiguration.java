58ff21068e4b0f667ac4ec912ed7ab6b1a77dfdf
duhacelik <dduuhhaa2306@gmail.com>
2024-07-18 23:17:15 +0300

    Spring boot security enabled at basic level

--- src/main/java/org/development/authms/configuration/SecurityConfiguration.java
+++ src/main/java/org/development/authms/configuration/SecurityConfiguration.java
@@ -3,24 +3,24 @@ package org.development.authms.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
-@Component
-public class WebSecurityConfiguration {
+@EnableWebSecurity
+public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
                         .anyRequest().permitAll()).csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(withDefaults());
+                .httpBasic(withDefaults()).logout(LogoutConfigurer::permitAll);
         return http.build();
     }
-
 }
